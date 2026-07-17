c5c29b91dbb82ff4dc09d1076eac6eae8511e38e
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-17 17:27:58 -0300

    refactor: adiciona validacao para o valor da gorjeta

--- projeto-01-calculadora-gorjeta/main.py
+++ projeto-01-calculadora-gorjeta/main.py
@@ -11,11 +11,24 @@ def ler_valor_conta(mensagem):
             return valor
         except ValueError:
             print(
-            "ERRO! Porfavor digite um número válido(Sem letras, caracteres ou espaços em branco)"
+            "ERRO! Digite apenas números."
+            )
+            
+def ler_valor_gorjeta(mensagem):
+    while True:
+        try:
+            valor = float(input(mensagem))
+            if valor < 0:
+                print("O valor não pode ser negativo. Tente novamente.")
+                continue
+            return valor
+        except ValueError:
+            print(
+            "ERRO! Digite apenas números."
             )
 
 total_conta = ler_valor_conta("Total a pagar: R$ ")
-gorjeta = int(input("Porcentagem de gorjeta: "))
+gorjeta = ler_valor_gorjeta("Porcentagem de gorjeta: ")
 
 total_gorjeta = calcular_gorjeta(total_conta, gorjeta)
 

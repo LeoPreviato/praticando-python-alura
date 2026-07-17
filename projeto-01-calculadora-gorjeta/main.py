e6c80b8c9aa62c5027a7e3d7d5bb10d4ceaa84bc
Leonardo Previato <leonardopreviato0309@gmail.com>
2026-07-17 16:38:15 -0300

    refactor: adiciona validação para o valor da conta

--- projeto-01-calculadora-gorjeta/main.py
+++ projeto-01-calculadora-gorjeta/main.py
@@ -1,12 +1,24 @@
 def calcular_gorjeta(valor_conta, porcentagem):
     return valor_conta * (porcentagem / 100)
 
-total_conta = float(input("Total a pagar: R$ "))
+def ler_valor_conta(mensagem):
+    while True:
+        try:
+            valor = float(input(mensagem))
+            if valor <= 0:
+                print("O valor não pode ser zero ou negativo. Tente novamente.")
+                continue
+            return valor
+        except ValueError:
+            print(
+            "ERRO! Porfavor digite um número válido(Sem letras, caracteres ou espaços em branco)"
+            )
+
+total_conta = ler_valor_conta("Total a pagar: R$ ")
 gorjeta = int(input("Porcentagem de gorjeta: "))
 
 total_gorjeta = calcular_gorjeta(total_conta, gorjeta)
 
 print()
-
 print(f"Valor da gorjeta: R$ {total_gorjeta:.2f}")
 print(f"Total a pagar: R$ {total_conta + total_gorjeta:.2f}")
